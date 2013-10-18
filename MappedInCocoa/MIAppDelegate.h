14642b1dc96cb3902a0a21b455bb582f68c4ee9d
Dan Lichty <djlichty@gmail.com>
2013-10-18 16:05:21 -0400

    add license and empty Xcode project

---
+++ MappedInCocoa/MIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MIAppDelegate.h
+//  MappedInCocoa
+//
+//  Created by Dan Lichty on 2013-10-18.
+//  Copyright (c) 2013 MappedIn, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MappedInCocoa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MappedInCocoa
+//
+//  Created by Dan Lichty on 2013-10-18.
+//  Copyright (c) 2013 MappedIn, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([MIAppDelegate class]));
+  }
+}
