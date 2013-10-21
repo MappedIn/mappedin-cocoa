88845601689389b262461b58dc90105f1cebc0e4
Dan Lichty <djlichty@gmail.com>
2013-10-21 12:44:35 -0400

    add empty API implementation

---
+++ MappedInCocoa/Classes/MIAPI.h
@@ -0,0 +1,13 @@
+//
+//  MIAPI.h
+//  MappedInCocoa
+//
+//  Created by Dan Lichty on 2013-10-21.
+//  Copyright (c) 2013 MappedIn, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MIAPI : NSObject
+
+@end
---
+++ MappedInCocoa/Classes/MIAPI.m
@@ -0,0 +1,13 @@
+//
+//  MIAPI.m
+//  MappedInCocoa
+//
+//  Created by Dan Lichty on 2013-10-21.
+//  Copyright (c) 2013 MappedIn, Inc. All rights reserved.
+//
+
+#import "MIAPI.h"
+
+@implementation MIAPI
+
+@end
